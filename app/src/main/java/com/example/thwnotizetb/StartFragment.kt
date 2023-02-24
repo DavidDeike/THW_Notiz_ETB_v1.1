6396ee0a35442bcdb9e32648620eaa78fcf57d80
David Deike <117454601+DavidDeike@users.noreply.github.com>
2023-02-24 16:28:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/thwnotizetb/StartFragment.kt
@@ -0,0 +1,44 @@
+package com.example.thwnotizetb
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.thwnotizetb.databinding.StartFragmentBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: StartFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = StartFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.Start.setOnClickListener {
+            findNavController().navigate(R.id.a)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
